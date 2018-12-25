6bbdabd4c30b56d30e04bd5f87e6f18e0104dd33
Nathan Ross <NathanAnthonyRoss19@gmail.com>
2018-12-24 17:54:47 -0800

    Added basic scoring system

--- water.js
+++ water.js
@@ -3,6 +3,9 @@
  *  Created 12/15/18
  */
 
+const scoreBoard = document.getElementById("score-board");
+const playAgainBtn = document.getElementById("play-again");
+
 const images = [
     "ali.jpg",
     "flower.jpg",
@@ -12,36 +15,78 @@ const images = [
     "rosquillas.jpg",
     "tem.jpg",
     "tux.jpg",
-]
+];
+
+const scratchedImages = [];
+let score = 0;
+
+function addScore(increment) {
+    score += increment;
+
+    scoreBoard.innerText = "score: " + score;
+}
+addScore(500);
+
+
+
+function refreshBoard() {
+    for (const canvas of document.querySelectorAll("canvas")) {
+        const context = canvas.getContext("2d");
+        canvas.segmentsErased = 0;
+    
+        //randomly select a background image
+        canvas.imageIndex = Math.floor(Math.random() * images.length);
+        const imageUrl = images[canvas.imageIndex];
+        canvas.style.backgroundImage = `url("${imageUrl}")`;
+    
+        context.globalCompositeOperation = "source-over";
+    
+        //completely fill canvas with a solid color
+        context.fillStyle = "white";
+        context.rect(0, 0, canvas.width, canvas.height);
+        context.fill();
+        
+        //draw text to suggest desired behavior
+        context.fillStyle = "black";
+        context.font = "30px sans";
+        context.textAlign = "center";
+        context.fillText("Scratch me!", canvas.width / 2, canvas.height / 2);
+    
+        //lines are rounded at each end
+        context.lineCap = "round"; //"butt", "round", "square"
+    
+        //further drawing actions will erase the solid coloring from the canvas
+        context.globalCompositeOperation = "destination-out"; //erase instead of drawing
+    }
+}
+refreshBoard();
 
 for (const canvas of document.querySelectorAll("canvas")) {
-    const context = canvas.getContext("2d");
-
-    //drawing commands to perform when a mouse or finger movies
+    //drawing commands to perform when a mouse or finger moves
     canvas.oncursormove = function(x, y, prevX, prevY) {
         const context = this.getContext("2d");
-        drawLine(context, 30, prevX, prevY, x, y);
+        drawLine(context, 50, prevX, prevY, x, y);
+
+        ++canvas.segmentsErased;
+        if (canvas.segmentsErased == 30) {
+            let matches = 0;
+            for (const imageIndex of scratchedImages) {
+                if (imageIndex === canvas.imageIndex) {
+                    ++matches;
+                }
+            }
+
+            scratchedImages.push(this.imageIndex);
+            if (matches > 0) {
+                addScore(Math.pow(10, matches));
+            }
+        }
     }
-
-    //randomly select a background color
-    const imageIndex = Math.floor(Math.random() * images.length);
-    canvas.style.backgroundImage = `url("${images[imageIndex]}")`;
-
-    //completely fill canvas with a solid color
-    context.fillStyle = "white";
-    context.rect(0, 0, canvas.width, canvas.height);
-    context.fill();
-    
-    //draw text to suggest desired behavior
-    context.fillStyle = "black";
-    context.font = "30px sans";
-    context.textAlign = "center";
-    context.fillText("Scratch me!", canvas.width / 2, canvas.height / 2);
-
-    //lines are rounded at each end
-    context.lineCap = "round"; //"butt", "round", "square"
-
-    //further drawing actions will erase the solid coloring from the canvas
-    context.globalCompositeOperation = 'destination-out'; //erase instead of drawing
-
 }
+
+
+playAgainBtn.addEventListener("click", function(event) {
+    addScore(-10);
+    scratchedImages.length = 0;
+    refreshBoard();
+});
